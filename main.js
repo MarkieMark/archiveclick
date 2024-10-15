e04445b66a7fe9d314dc09ce62152048107443b8
mark <MarkBenjamin1@gatech.edu>
2024-10-15 12:25:49 +0300

    options for rclick new tab, fg/bg; shift/ctrl override

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-openInArchive = function(data){
+openInArchive = function(data) {
   var url = data.linkUrl;
-  browser.tabs.create({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(url), active: false});
+  var shift = (data.modifiers.indexOf('Shift') > -1);
+  var ctrl = ((data.modifiers.indexOf('Ctrl') > -1) || (data.modifiers.indexOf('MacCtrl') > -1));
+  browser.storage.sync.get({
+      sameTab: true,
+      backgroundNewTab: true,
+      rightClickNewTab: true
+    }).then((items) => {
+    if (items.rightClickNewTab != shift) {
+      browser.tabs.create({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(url), active: (items.backgroundNewTab == ctrl)});
+    } else {
+      browser.tabs.update({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(url)});
+    }
+  });
 };
 
-archiveTab = function (tab) {
-  browser.storage.sync.get({ sameTab: true }).then((items) => {
-    if (items.sameTab == true) {
+archiveTab = function (tab, data) {
+  var shift = (data.modifiers.indexOf('Shift') > -1);
+  var ctrl = ((data.modifiers.indexOf('Ctrl') > -1) || (data.modifiers.indexOf('MacCtrl') > -1));
+  browser.storage.sync.get({
+      sameTab: true,
+      backgroundNewTab: true,
+      rightClickNewTab: true
+    }).then((items) => {
+    if (items.sameTab != shift) {
       browser.tabs.update({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(tab.url)});
     } else {
-      browser.tabs.create({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(tab.url), active: false});
+      browser.tabs.create({url: "https://archive.ph/?run=1&url=" + encodeURIComponent(tab.url), active: (items.backgroundNewTab == ctrl)});
     }
   });
 };
@@ -17,10 +35,10 @@ archiveTab = function (tab) {
 browser.contextMenus.removeAll();
 
 browser.contextMenus.create({
-    id: 'archive_ph_link1',
-    title: 'Archive Link',
-    contexts:['link'],
-  });
+  id: 'archive_ph_link1',
+  title: 'Archive Link',
+  contexts: ['link'],
+});
 
 browser.contextMenus.onClicked.addListener(openInArchive);
 
--- options.js
+++ options.js
@@ -1,9 +1,15 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
   const sameTab = document.getElementById('same-tab').checked;
+  const backgroundNewTab = document.getElementById('background-new-tab').checked;
+  const rightClickNewTab = document.getElementById('right-click-new-tab').checked;
 
   browser.storage.sync.set(
-    { sameTab: sameTab },
+    {
+      sameTab: sameTab,
+      backgroundNewTab: backgroundNewTab,
+      rightClickNewTab: rightClickNewTab
+    },
     () => {
       // Update status to let user know options were saved.
       const status = document.getElementById('status');;
@@ -15,23 +21,25 @@ const saveOptions = () => {
   );
 };
 
-// Restores select box and checkbox state using the preferences
-// stored in browser.storage.
+// Restores checkbox state using the stored preferences
 const restoreOptions = () => {
   browser.storage.sync.get(
-    { sameTab: true },
+    {
+      sameTab: true,
+      backgroundNewTab: true,
+      rightClickNewTab: true
+    },
     (items) => {
       console.log('options items:');
       console.log(items);
       document.getElementById('same-tab').checked = items.sameTab;
+      document.getElementById('background-new-tab').checked = items.backgroundNewTab;
+      document.getElementById('right-click-new-tab').checked = items.rightClickNewTab;
     }
   );
 };
 
-const closeOptions = () => {
-  window.close();
-};
-
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('same-tab').addEventListener('click', saveOptions);
-document.getElementById('close-button').addEventListener('click', closeOptions);
+document.getElementById('background-new-tab').addEventListener('click', saveOptions);
+document.getElementById('right-click-new-tab').addEventListener('click', saveOptions);
